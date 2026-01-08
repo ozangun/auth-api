1b1ad8f6ede8261b5bd9fdfd9a4912a43d224126
ozangun <ozangun77@gmail.com>
2026-01-08 16:59:22 +0300

    db and app.js created and db connection

---
+++ app.js
@@ -0,0 +1,6 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+const db = require('./config/db.js');
+app.listen(port, () => {
+});
---
+++ config/db.js
@@ -0,0 +1,11 @@
+const dotenv = require('dotenv');
+dotenv.config({ path: './.env' });
+const { Pool } = require("pg")
+const pool = new Pool({
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  host: process.env.DB_HOST,
+  port: Number(process.env.DB_PORT),
+  database: process.env.DB_NAME,
+});
+module.exports = pool;
